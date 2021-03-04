e39ce8580ca3696598c79094348fa51ae60c51dd
Alex Santos <alexdsantosv@gmail.com>
2021-03-03 21:23:29 -0600

    [ Add ] regexp to sections

--- files/parser.py
+++ files/parser.py
@@ -18,7 +18,7 @@ def add_error(errs, err_index, groups, line, line_number, msg):
 def find_restaurants(res_names, line, line_number, errs):
     # SEARCH
     res_declarations = re.search(
-        "^\s*(\w+)\s*(\W)\s*\'*([^\']*)?\'*", line)
+        "^\s*(\w+)\s*(\W)\s*(\'*([^\']*)?\'*)", line)
 
     if(res_declarations):
         # GRUPOS
@@ -30,37 +30,65 @@ def find_restaurants(res_names, line, line_number, errs):
                       "debe ser \'restaurante\'")
 
         # ERROR AL IGUALAR
-        elif(not re.match("^=$", groups[1], re.IGNORECASE)):
+        elif(groups[1] != '='):
             add_error(errs, 1, groups, line, line_number,
                       "debe ser \'=\'")
 
         # ERROR AL ASIGNAR
-        elif(not re.match("^[a-zA-Z]+$", groups[2], re.IGNORECASE)):
+        elif(not re.match("'[^']*'", groups[2], re.IGNORECASE)):
             add_error(errs, 2, groups, line, line_number,
                       "debe ser un string")
 
         # AGREGAR RESTAURANTE
         else:
-            res_names.append(groups[2])
+            res_names.append(groups[2].replace('\'', ''))
 
-    return res_names
+
+def find_sections(res_sections, line, line_number, errs):
+    # SEARCH
+    res_declarations = re.search(
+        "^\s*('([^']*)'*)\s*(\W)", line)
+
+    if(res_declarations):
+        # GRUPOS
+        groups = res_declarations.groups()
+
+        # ERROR AL IGUALAR
+        if(groups[2] != ':'):
+            add_error(errs, 2, groups, line, line_number,
+                      "debe ser \':\'")
+
+        # ERROR DE COMILLAS
+        elif(not re.match("'[^']*'$", groups[0], re.IGNORECASE)):
+            add_error(errs, 0, groups, line, line_number,
+                      "falto cerrar comillas")
+
+        # AGREGAR RESTAURANTE
+        else:
+            res_sections.append(groups[0].replace('\'', ''))
+
+    return res_sections
 
 
 def parse_menu_files(lines):
-    # LISTA DE ERRORES
+    # LISTA DE ERRORES Y RESTAURANTES
     errs = []
     res_names = []
+    res_sections = []
 
+    # RECCORER
     line_number = 1
-
     for line in lines.split('\n'):
         # BUSCAR NOMBRES DE RESTAURANTES
         find_restaurants(res_names, line, line_number, errs)
 
+        # BUSCAR NOMBRES DE SECCIONES
+        find_sections(res_sections, line, line_number, errs)
+
         # AUMENTAR LINEA
         line_number += 1
 
-    print(res_names)
+    print(res_names, res_sections)
 
     # MOSTRAR ERRORES
     print('\n')
